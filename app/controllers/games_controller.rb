6231a89c56a22f3227fef1c50cc348ff0670ee8a
Eddie Melo <heriberto@melo.nyc>
2018-05-15 18:55:12 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    @game = Game.new
+    @game = Game.create(game_params)
+    render json: @game, status: 201
   end
 
   def show
